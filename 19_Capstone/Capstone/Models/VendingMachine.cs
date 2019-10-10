6c46cd2b80d04d78e029898857525f2eebea8284
William Schneider <Rose2wet@gmail.com>
2019-10-10 11:14:07 -0400

    small mods to stock method

--- 19_Capstone/Capstone/Models/VendingMachine.cs
+++ 19_Capstone/Capstone/Models/VendingMachine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Capstone.Models
@@ -7,7 +8,7 @@ namespace Capstone.Models
     public class VendingMachine
     {
         // TODO Relevant methods should update this
-        private decimal balance;
+        public decimal Balance;
 
         // TODO Assign this in constructor
         private readonly string inputFilePath;
@@ -26,13 +27,59 @@ namespace Capstone.Models
         public List<Slot> Slots { get; private set; }
 
 
+
+        //constructor
+        
+        public VendingMachine(string inputFilePath) 
+        {
+            this.inputFilePath = inputFilePath;
+            Stock();
+        }
+
+
         // TODO Set up summary
         // TODO Determine return value
-        public void Stock()
+        public bool Stock()
         {
+            List<string> inputLines = new List<string>();
             // TODO Read from input file
             // TODO Create Product instances
             // TODO Add Products to Products
+            //TODO bool?
+            try
+            {
+                
+                using (StreamReader sr = new StreamReader(inputFilePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        inputLines.Add(sr.ReadLine()); 
+
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                //TODO dont forget about this
+                 return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            foreach(string line in inputLines)
+            {
+                //TODO price Decimal error alert input file
+                string[] stockElements = line.Split("|");
+                string identifier = stockElements[0];
+                string nameProduct = stockElements[1];
+                string priceProduct = stockElements[2];
+                string productClass = stockElements[3];
+
+                if (Products.IndexOf)
+            }
+            return true;
         }
 
         // TODO Set up summary
