03f608e64a247945ff50dd44acc628117029a73c
Reid Larson <reid.d.larson@gmail.com>
2019-10-10 09:18:39 -0400

    Product class outline

---
+++ 19_Capstone/Capstone/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    abstract class Product
+    {
+        public string Name { get; }
+        public string Price { get; }
+        private int quantitySold;
+
+        // TODO Override this method
+        public abstract void YumYum();
+    }
+}
