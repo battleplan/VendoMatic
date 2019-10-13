5f9421a0e609305643f6849a6889925ff9269cbc
Reid Larson <reid.d.larson@gmail.com>
2019-10-13 12:40:21 -0400

    Parentheses instead of pipes designate slot quantity

--- 19_Capstone/Capstone/Views/CLIMenu.cs
+++ 19_Capstone/Capstone/Views/CLIMenu.cs
@@ -228,7 +228,7 @@ namespace Capstone.Views
         {
             if (slot.HasStock)
             {
-                return $"{slot.Price:C} |{slot.QuantityRemaining}| {slot.Product.Name}";
+                return $"{slot.Price:C} ({slot.QuantityRemaining}) {slot.Product.Name}";
             }
             else
             {
